bef1aea559174d91aa3dc85060c45519ca6581be
pupubird <rainchai4240@gmail.com>
2020-06-04 17:14:10 +0800

    added counter app example

---
+++ CounterApp/final/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import FancyButton from "./components/Fancybutton.svelte";
+  let label = "Add counter value!";
+  let value = 0;
+
+  function add() {
+    value += 1;
+  }
+</script>
+
+<style>
+  .center {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
+
+<div class="center">
+  <h1>{value}</h1>
+  <FancyButton {label} callback={add} />
+</div>
---
+++ CounterApp/final/src/components/Fancybutton.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let label = "Click me";
+  export let callback = () => console.log("hi");
+</script>
+
+<style>
+  button {
+    cursor: pointer;
+    background-color: #4caf50;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+  }
+</style>
+
+<button on:click={callback}>{label}</button>
